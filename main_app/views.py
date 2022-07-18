a2689df3b3e79b073644aa8602a708d0d5a1b9ec
Madhumida <smadhumida@gmail.com>
2022-07-18 12:40:55 -0700

    Index and Dog details

--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Dog
 
 # Create your views here.
 
 
-class Dog:  # Note that parens are optional if not inheriting from another class
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
+# class Dog:  # Note that parens are optional if not inheriting from another class
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
 
 
-dogs = [
-    Dog('Ziva', 'Maltipoo', 'Honey Bunch Sugar Sweet', 1),
-    Dog('Zara', 'Yorkiepoo', 'Charming Face', 2),
-    Dog('Lulu', 'Husky', 'Ferocious Look', 4)
-]
+# dogs = [
+#     Dog('Ziva', 'Maltipoo', 'Honey Bunch Sugar Sweet', 1),
+#     Dog('Zara', 'Yorkiepoo', 'Charming Face', 2),
+#     Dog('Lulu', 'Husky', 'Ferocious Look', 4)
+# ]
 
 
 def home(request):
@@ -28,4 +29,10 @@ def about(request):
 
 
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {'dogs': dogs})
+
+
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    return render(request, 'dogs/detail.html', {'dog': dog})
