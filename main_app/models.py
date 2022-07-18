791b7df5a997cc2475111c80f2a32b3f5227ddf4
Madhumida <smadhumida@gmail.com>
2022-07-18 13:03:29 -0700

    Adds crud operations

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
 
 # Create your models here.
 
@@ -11,3 +13,6 @@ class Dog(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'dog_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Dog
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 
 # Create your views here.
 
@@ -36,3 +38,20 @@ def dogs_index(request):
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
     return render(request, 'dogs/detail.html', {'dog': dog})
+
+
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+    success_url = '/dogs/'
+
+
+class DogUpdate(UpdateView):
+    model = Dog
+    # Let's disallow the renaming of a dog by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogs/'
